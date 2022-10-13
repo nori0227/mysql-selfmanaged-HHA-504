875aeddafd438fc7c434d0537a3f5c0eaaea8b71
Jannatul Ferdous <110571832+nori0227@users.noreply.github.com>
2022-10-12 20:47:06 -0400

    Update mysql_selfmanaged.py

--- mysql_selfmanaged.py
+++ mysql_selfmanaged.py
@@ -30,4 +30,4 @@ db1
 collected_data = pd.read_csv('/State_Taxes_and_Fees_Collected__Beginning_Fiscal_Year_Ending_March_31__1995.csv')
 collected_data
 
-collected_data.to_sql('/Taxes_and_Fees_Collected_Fiscal_Year__1995', con= db1)
+collected_data.to_sql('Taxes_and_Fees_Collected_Fiscal_Year__1995', con= db1)
